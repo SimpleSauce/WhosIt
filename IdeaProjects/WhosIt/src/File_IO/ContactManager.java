1021f9ce385bd11425f75a58bdcdf47b663218eb
JSR7 <G!thUb1!>
2017-05-25 14:59:34 -0500

    added needed info to Contact Manager exercise

--- IdeaProjects/WhosIt/src/File_IO/ContactManager.java
+++ IdeaProjects/WhosIt/src/File_IO/ContactManager.java
@@ -8,7 +8,14 @@ public class ContactManager {
 
     Scanner scan = new Scanner(System.in);
 
-
+    public static void createMenu (){
+        System.out.println("1. View Contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5)");
+    }
 
     public static void main(String[] args) {
         String directory = "data";
@@ -26,5 +33,10 @@ public class ContactManager {
         }
 
     }
+
+
+
+
+
 
 }
