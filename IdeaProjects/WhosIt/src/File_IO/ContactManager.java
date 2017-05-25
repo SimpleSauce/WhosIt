7ed63ab36151c6c393f72be70b868677fb7921a0
Jeriko <jcarrico1985@live.com>
2017-05-25 15:53:38 -0500

    Created PrintStream type 'print'.
    Created following methods:
    
    viewContacts()
    addContact()
    searchName()
    deleteContact()
    
    Created switch cases for 1-4 + default of user input.

--- IdeaProjects/WhosIt/src/File_IO/ContactManager.java
+++ IdeaProjects/WhosIt/src/File_IO/ContactManager.java
@@ -1,47 +1,89 @@
 package File_IO;
+
 import java.nio.file.*;
 import java.util.*;
 import java.io.*;
-import java.nio.*;
 
 public class ContactManager {
 
-    Scanner scan = new Scanner(System.in);
+    private static Scanner scan = new Scanner(System.in);
+    private static PrintStream print = System.out;
+    private static String directory = "data";
+    private static String filename = "contacts.txt";
+    private static Path dataDirectory = Paths.get(directory);
+    private static Path dataFile = Paths.get(directory, filename);
+    private static String input = scan.nextLine();
 
-    public static void createMenu (){
-        System.out.println("1. View Contacts.");
-        System.out.println("2. Add a new contact.");
-        System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
-        System.out.println("5. Exit.");
-        System.out.println("Enter an option (1, 2, 3, 4 or 5)");
+    private static void viewContacts() {
+
+    }
+
+    public static void createMenu() {
+        print.println("1. View Contacts.");
+        print.println("2. Add a new contact.");
+        print.println("3. Search a contact by name.");
+        print.println("4. Delete an existing contact.");
+        print.println("5. Exit.");
+        print.println("Enter an option (1, 2, 3, 4 or 5)");
+    }
+
+    public static void addContact() {
+        print.println("Type the name of the contact.");
+        try {
+            Path
+        } catch (Exception e) {
+            print.println("Whoopsie Something went wrong...Try again!");
+            addContact();
+        }
+
+    }
+
+    public static void searchName() {
+        print.println("Type the name of the contact to search.");
+    }
+
+    public static void deleteContact() {
+        print.println("Type the name of the contact that you want to delete.");
     }
 
     public static void main(String[] args) {
-        String directory = "data";
-        String filename = "contacts.txt";
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
+
 
         try {
             if (Files.notExists(dataDirectory)) {
-            Files.createDirectories(dataDirectory);
-        }} catch (Exception e){
-            System.out.println("Something went wrong.");
+                Files.createDirectories(dataDirectory);
+            }
+        } catch (Exception e) {
+            print.println("Something went wrong.");
         }
 
-try{if (Files.notExists(dataFile)) {
-    Files.createFile(dataFile);
-}} catch (Exception e){
-    System.out.println("Something is jacked up.");
-}
+        try {
+            if (Files.notExists(dataFile)) {
+                Files.createFile(dataFile);
+            }
+        } catch (Exception e) {
+            print.println("Something went wrong.");
+        }
 
+        createMenu();
+
+        switch(input) {
+            case "1": {
+                viewContacts();
+            } break;
+            case "2": {
+                addContact();
+            } break;
+            case "3": {
+                searchName();
+            } break;
+            case "4": {
+                deleteContact();
+            } break;
+            default: {System.exit(0);}
+        }
 
     }
-
-
-
-
 
 
 }
