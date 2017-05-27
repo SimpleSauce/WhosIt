7e9e366ad92a02b2f18d80fe9ca5345e2d92410b
Jeriko <jcarrico1985@live.com>
2017-05-27 14:04:18 -0500

    Working on method viewContacts()
    Working on method addContacts()

--- IdeaProjects/WhosIt/src/File_IO/ContactManager.java
+++ IdeaProjects/WhosIt/src/File_IO/ContactManager.java
@@ -12,10 +12,20 @@ public class ContactManager {
     private static String filename = "contacts.txt";
     private static Path dataDirectory = Paths.get(directory);
     private static Path dataFile = Paths.get(directory, filename);
-    private static String input = scan.nextLine();
+    private static List <String> lines;
 
     private static void viewContacts() {
+        lines = Arrays.asList();
 
+        try {
+            for (int i = 0; i<Files.readAllLines(dataFile).size(); i++) {
+            print.println(Files.readAllLines(dataFile).get(i));
+            }
+        }
+        catch (Exception e){
+            print.println("Hmm, something went terribly wrong...Let's try that again!");
+            createMenu();
+        }
     }
 
 
@@ -26,13 +36,45 @@ public class ContactManager {
         print.println("4. Delete an existing contact.");
         print.println("5. Exit.");
         print.println("Enter an option (1, 2, 3, 4 or 5)");
+        String input = scan.nextLine();
+        switch(input) {
+            case "1": {
+                viewContacts();
+            } break;
+            case "2": {
+                addContact();
+            } break;
+            case "3": {
+                searchName();
+            } break;
+            case "4": {
+                deleteContact();
+            } break;
+            default: {System.exit(0);}
+        }
     }
 
     public static void addContact() {
         print.println("Type the name of the contact.");
-        try {
-           Files.write(dataFile, new ArrayList<String>(), StandardOpenOption.APPEND);
-        } catch (Exception e) {
+        String input = scan.next();
+        print.println("Please input the phone number for this contact:");
+        String phone = scan.next();
+        print.println("Finally, please input this contact's e-mail address:");
+        String email = scan.next();
+        lines = Arrays.asList("Name: " + input + " " + " Phone Number: " + phone);
+        try
+        {
+           Files.write(dataFile, lines, StandardOpenOption.APPEND);
+
+           print.println("Add another contact?");
+           String input2 = scan.next();
+            if (input2.equalsIgnoreCase("Y")){addContact();}
+            else {
+                scan.nextLine();
+                createMenu();
+            }
+        }
+        catch (Exception e) {
             print.println("Whoopsie Something went wrong...Try again!");
             addContact();
         }
@@ -47,7 +89,6 @@ public class ContactManager {
     }
 
     public static void main(String[] args) {
-
 
         try {
             if (Files.notExists(dataDirectory)) {
@@ -66,22 +107,6 @@ public class ContactManager {
         }
 
         createMenu();
-
-        switch(input) {
-            case "1": {
-                viewContacts();
-            } break;
-            case "2": {
-                addContact();
-            } break;
-            case "3": {
-                searchName();
-            } break;
-            case "4": {
-                deleteContact();
-            } break;
-            default: {System.exit(0);}
-        }
 
     }
 
